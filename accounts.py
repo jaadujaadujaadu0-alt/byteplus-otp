fc4d238b5b602cb4c4d26ac974e0665aa751ecf1
jaadujaadujaadu0-alt <jaadujaadujaadu0@gmail.com>
2026-02-21 15:42:05 +0530

    Create accounts.py

---
+++ accounts.py
@@ -0,0 +1,9 @@
+# accounts.py
+ACCOUNTS = [
+    {"user": "irontmp+nlxsu@gmail.com", "pass": "irontmp+nlxsu@gmail.comA"},
+    {"user": "reynsia7+l0t3a@gmail.com", "pass": "Aa1+Aa1+@"},
+    {"user": "kentkouh+m4jma@gmail.com", "pass": "kentkouh+m4jma@gmail.comA"},
+    {"user": "irontmp+nlxsu@gmail.com", "pass": "irontmp+nlxsu@gmail.comA"},
+    {"user": "reynsia7+l0t3a@gmail.com", "pass": "Aa1+Aa1+@"},
+    {"user": "kentkouh+m4jma@gmail.com", "pass": "kentkouh+m4jma@gmail.comA"}
+]
